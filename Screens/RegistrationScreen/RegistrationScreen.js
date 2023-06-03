89022540da5b2ef2e91468ba4199393e6e31be30
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-04 02:11:37 +0300

    Create RegistrationScreen.js

---
+++ Screens/RegistrationScreen/RegistrationScreen.js
@@ -0,0 +1,177 @@
+import {
+  TextInput,
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  ImageBackground,
+  Button,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+} from "react-native";
+
+const RegistrationScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../RegistrationScreen/bgImage.png")}
+        resizeMode="cover"
+        style={styles.container}
+      >
+        <StatusBar
+          style="auto"
+          backgroundColor="#61dafb"
+          barStyle="dark-content"
+        />
+        <View style={styles.containerForm}>
+          <View style={styles.avatar}>
+            <Image
+              source={require("../RegistrationScreen/add.png")}
+              style={styles.addButton}
+            />
+          </View>
+          <Text style={styles.title}>Реєстрація</Text>
+          <TextInput placeholder="Логін" style={styles.inputLogin} />
+          <TextInput
+            placeholder="Адрес електронної пошти"
+            style={styles.inputEmail}
+          />
+          <TextInput placeholder="Пароль" style={styles.inputPass} />
+          <View style={styles.btnContainer}>
+            <Pressable style={styles.showBtn}>
+              <Text style={styles.showBtnText}>{"Показати"}</Text>
+            </Pressable>
+          </View>
+
+          <View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Зареєструватися</Text>
+            </TouchableOpacity>
+            <Pressable>
+              <Text style={styles.authText}>Уже є акаунт? Увійти</Text>
+            </Pressable>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    width: "100%",
+    alignItems: "center",
+  },
+  avatar: {
+    position: "absolute",
+    left: 128,
+    top: -60,
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
+  addButton: {
+    position: "absolute",
+    width: 25,
+    height: 25,
+    left: 106,
+    top: 79,
+  },
+  title: {
+    textAlign: "center",
+    fontFamily: "Roboto500",
+    fontSize: 30,
+    lineHeight: 35,
+    marginBottom: 33,
+    color: "#212121",
+  },
+  containerForm: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 92,
+    paddingBottom: 45,
+    backgroundColor: "#fff",
+    width: "100%",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  inputLogin: {
+    height: 50,
+    marginBottom: 16,
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+    padding: 16,
+  },
+  inputEmail: {
+    height: 50,
+    marginBottom: 16,
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+    padding: 16,
+  },
+  inputPass: {
+    height: 50,
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+    padding: 16,
+  },
+
+  button: {
+    alignItems: "center",
+
+    paddingTop: 16,
+    paddingBottom: 16,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+    marginTop: 43,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    lineHeight: 19,
+    fontFamily: "Roboto400",
+  },
+  authText: {
+    textAlign: "center",
+    marginTop: 16,
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+
+  showBtn: {
+    position: "absolute",
+    top: -35,
+    right: 16,
+  },
+  showBtnText: {
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
+export default RegistrationScreen;
