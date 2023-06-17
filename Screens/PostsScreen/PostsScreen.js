e0f65ed87e70bae2c50d78f37fd02ed9b87c7a08
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-18 02:50:47 +0300

    Update PostsScreen.js

--- Screens/PostsScreen/PostsScreen.js
+++ Screens/PostsScreen/PostsScreen.js
@@ -1,7 +1,11 @@
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
 import { View, Image, Text } from "react-native";
 import { StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
 export function PostsScreen() {
-    
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.userInfo}>
@@ -14,6 +18,33 @@ export function PostsScreen() {
           <Text style={styles.userEmail}>email@example.com</Text>
         </View>
       </View>
+      <ScrollView>
+        <View>
+          <View style={styles.imageWrapper}>
+            <Image source={require("../Home/natureImage-1.png")} />
+          </View>
+          <Text style={styles.imageText}>Ліс</Text>
+        </View>
+        <View style={styles.detailsWrapper}>
+          <TouchableOpacity onPress={() => navigation.navigate("Comments")}>
+            <Image
+              style={styles.shapeIcon}
+              source={require("../Home/shape.png")}
+            />
+          </TouchableOpacity>
+          <Text style={styles.countComents}>0</Text>
+          <Image
+            style={styles.mapPinIcon}
+            source={require("../CreatePostsScreen/map-pin.png")}
+          />
+          <Text>Ivano-Frankivs'k Region, Ukraine</Text>
+        </View>
+        <View>
+          <View style={styles.imageWrapper}>
+            <Image source={require("../Home/natureImage-2.png")} />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -29,6 +60,7 @@ const styles = StyleSheet.create({
   userInfo: {
     flexDirection: "row",
     alignItems: "center",
+    
   },
   userPhoto: {
     width: 60,
@@ -47,5 +79,40 @@ const styles = StyleSheet.create({
     fontSize: 11,
     lineHeight: 13,
     color: "rgba(33, 33, 33, 0.8)",
+  },
+  imageWrapper: {
+    marginTop: 32,
+    borderRadius: 8,
+
+    
+  },
+  imageText: {
+    marginTop: 8,
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  detailsWrapper: {
+    flexDirection: "row",
+    marginTop: 8,
+    alignItems: "center",
+    
+  },
+  shapeIcon: {
+    width: 18,
+    height: 18,
+    marginRight: 6,
+  },
+  mapPinIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+  },
+  countComents: {
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+    marginRight: 65,
   },
 });
