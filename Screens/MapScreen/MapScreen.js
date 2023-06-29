ba5120aa954580bea602e20ca02076470caa8b41
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-30 01:56:29 +0300

    Create MapScreen.js

---
+++ Screens/MapScreen/MapScreen.js
@@ -0,0 +1,38 @@
+import { View } from "react-native";
+import { Dimensions } from "react-native";
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+const MapScreen = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          ...location,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+      >
+        {location && (
+          <Marker title="I am here" coordinate={location} description="Hello" />
+        )}
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
+
+export default MapScreen;
